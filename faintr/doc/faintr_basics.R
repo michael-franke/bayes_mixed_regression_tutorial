c7760b281a199750b73f329a2c3a16431413f885
michael-franke <mchfranke@gmail.com>
2019-05-06 12:24:12 +0200

    added compiled vignettes

---
+++ faintr/doc/faintr_basics.R
@@ -0,0 +1,49 @@
+## ----setup, include=FALSE, echo = FALSE, message = FALSE-----------------
+knitr::opts_chunk$set(echo=TRUE, 
+                      warning=FALSE,
+                      message=FALSE, 
+                      collapse = TRUE,
+                      cache = TRUE,
+                      dev.args = list(bg = 'transparent'), 
+                      fig.align='center', 
+                      fig.height = 3, 
+                      fig.widht=4)
+library(tidyverse)
+theme_set(theme_bw() + theme(plot.background=element_blank()) )
+
+## ---- eval = F-----------------------------------------------------------
+#  devtools::install_github('michael-franke/bayes_mixed_regression_tutorial/faintr')
+#  library(faintr)
+
+## ---- echo = F-----------------------------------------------------------
+library(faintr)
+
+## ---- error=FALSE, warning=FALSE, message=FALSE--------------------------
+library(tidyverse)
+politedata = read_csv('https://raw.githubusercontent.com/michael-franke/bayes_mixed_regression_tutorial/master/code/politeness_data.csv') 
+head(politedata)
+
+## ------------------------------------------------------------------------
+politedata %>% group_by(gender, context) %>% summarize(mean_pitch = mean(pitch))
+
+## ---- error=FALSE, warning=FALSE, message=FALSE, results="hide"----------
+library(brms)
+m_dummy = brm(pitch ~ gender * context + (1 | subject + sentence), politedata)
+
+## ------------------------------------------------------------------------
+fixef(m_dummy)
+
+## ------------------------------------------------------------------------
+compare_groups(
+  model = m_dummy, 
+  higher = list(gender = "F", context = "pol"),
+  lower = list(gender = "M", context = "inf")
+)
+
+## ------------------------------------------------------------------------
+compare_groups(
+  model = m_dummy, 
+  higher = list(gender = "F"),
+  lower = list()
+)
+
